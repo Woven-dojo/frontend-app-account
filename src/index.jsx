c08331ff44372de315e85fac5220dc19d5d32b51
Tet <tetiana_brzezinska@bonify.de>
2022-11-04 12:58:37 +0100

    DSC-80 added header from dojo-frontend-common

--- src/index.jsx
+++ src/index.jsx
@@ -9,9 +9,10 @@ import {
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Switch } from 'react-router-dom';
+import { getConfig } from '@edx/frontend-platform/config';
+import { getAuthenticatedUser } from '@edx/frontend-platform/auth';
 
-import Header, { messages as headerMessages } from '@edx/frontend-component-header';
-import { Footer } from '@woven-dojo/dojo-frontend-common/dist/components';
+import { Header, Footer } from '@woven-dojo/dojo-frontend-common/dist/components';
 
 import configureStore from './data/configureStore';
 import AccountSettingsPage, { NotFoundPage } from './account-settings';
@@ -22,12 +23,47 @@ import appMessages from './i18n';
 import './index.scss';
 
 subscribe(APP_READY, () => {
+  const { BASE_URL, ORDER_HISTORY_URL, LOGOUT_URL } = getConfig();
+  const { username } = getAuthenticatedUser();
   ReactDOM.render(
     <AppProvider store={configureStore()}>
       <Switch>
         <Route path="/coaching_consent" component={CoachingConsent} />
         <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
-          <Header />
+          <Header
+            logoDestination={`${BASE_URL}/dashboard`}
+            logoDestinationTarget='_self'
+            username={username}
+            userMenu={[
+              {
+                label: 'Dashboard',
+                href: `${BASE_URL}/dashboard`,
+              },
+              {
+                label: 'Profile',
+                href: `${BASE_URL}/u/${username}`,
+              },
+              {
+                label: 'Account',
+                href: `${BASE_URL}/account/settings`,
+              },
+              {
+                label: 'Order History',
+                href: ORDER_HISTORY_URL,
+              },
+              {
+                label: 'Logout',
+                href: LOGOUT_URL,
+              },
+            ]}
+            actionMenu={[
+              {
+                label: 'Courses',
+                href: `${BASE_URL}/dashboard`,
+                target: '_self'
+              },
+            ]}
+          />
           <main className="flex-grow-1">
             <Switch>
               <Route path="/id-verification" component={IdVerificationPage} />
@@ -49,10 +85,7 @@ subscribe(APP_INIT_ERROR, (error) => {
 });
 
 initialize({
-  messages: [
-    appMessages,
-    headerMessages,
-  ],
+  messages: [appMessages],
   requireAuthenticatedUser: true,
   hydrateAuthenticatedUser: true,
   handlers: {
